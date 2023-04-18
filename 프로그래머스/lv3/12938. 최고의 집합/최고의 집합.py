ee67a8f781326a64a4988b4697e94b8619b5800e
EBAB <88013439+SonJinHYo@users.noreply.github.com>
2023-04-18 22:28:51 +0900

    [level 3] Title: 최고의 집합, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.py
@@ -0,0 +1,9 @@
+def solution(n, s):
+    if n>s:
+        return [-1]
+    
+    if s%n==0:
+        return [s//n for _ in range(n)]
+    else:
+        return [s//n for _ in range(n-s%n)]+[s//n+1 for _ in range(s%n)]
+        
